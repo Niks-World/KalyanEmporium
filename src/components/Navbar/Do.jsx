89b6153226e7e08fe7694083d0711dc2f47a7385
Nikhil Singh <79000068+Niks-World@users.noreply.github.com>
2025-06-30 14:49:32 +0530

    Update Do.jsx

--- src/components/Navbar/Do.jsx
+++ src/components/Navbar/Do.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Do.css";
+import imgmain from "../Assets/img/mainbaner.jpeg";
 
 function Do() {
   return (
     <section className="what-we-do" id="what-we-do">
       <h2>What We Do</h2>
       <p>We blend tradition with modern design to bring you authentic products.</p>
-      <img src="/assets/what-we-do.jpg" alt="What We Do" />
+      <img src={imgmain} alt="What We Do" />
     </section>
   );
 }
